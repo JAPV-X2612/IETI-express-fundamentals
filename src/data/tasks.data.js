9a93c72eaf4899ffd19a9b86112dee47f40174b8
JesusPinzon-Sainapsis <jesus.vega@sainapsis.com>
2026-02-18 23:05:03 -0500

    feat: Add new tasks data

---
+++ src/data/tasks.data.js
@@ -0,0 +1,7 @@
+const tasks = [
+    { id: 1, title: "Buy groceries", completed: false },
+    { id: 2, title: "Read a book", completed: true },
+    { id: 3, title: "Write tests", completed: false },
+];
+
+module.exports = tasks;
