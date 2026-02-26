a88945294e503315237123949ce99f0f91d35c5d
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-02-25 23:55:16 -0500

    feat: Solution JWW laboratory

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const users = [
   { email: "admin@example.com", name: "admin", rol: "admin" },
@@ -11,12 +19,42 @@ app.get("/", function (req, res) {
   res.send("Bienvenido a la api de ADA Cars");
 });
 
+app.post("/auth", (req, res) => {
+  const user = users.find((u) => u.email === req.body.email);
+  if (!user) {
+    return res.status(401).send({ error: "Invalid user name or password" });
+  }
+  const token = jwt.sign({ email: user.email, name: user.name, rol: user.rol }, SECRET_KEY);
+  res.json({ token });
+});
 
-//Se recomienda no editar ni eliminar la instancia del servidor.
-// Instancia del servidor
+const JWTValidation = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.rol = decoded.rol;
+    next();
+  } catch (error) {
+    res.json({ error });
+  }
+};
+
+app.get("/premium-clients", JWTValidation, (req, res) => {
+  if (req.rol === "admin") {
+    return res.json({ message: "premium-clients list" });
+  }
+  res.status(403).json({ error: "Access not allowed" });
+});
+
+app.get("/medium-clients", JWTValidation, (req, res) => {
+  if (req.rol === "admin" || req.rol === "user") {
+    return res.json({ message: "medium-clients list" });
+  }
+  res.status(403).json({ error: "Access not allowed" });
+});
+
 const server = app.listen(PORT, () => {
   console.log(`listening on port http://localhost:${PORT}`);
 });
 
-// Exportación del servidor
 module.exports = server;
