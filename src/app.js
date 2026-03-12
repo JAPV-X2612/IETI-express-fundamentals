69ea2c9453c905167294dbcf5f28cdf5298050a1
JesusPinzon-Sainapsis <jesus.vega@sainapsis.com>
2026-03-12 00:28:27 -0500

    feat: Add new endpoints logic

--- src/app.js
+++ src/app.js
@@ -5,11 +5,20 @@ const premiumClientsEndpoint = require("./premium-clients");
 
 app.use(express.json());
 
+app.get("/medium-clients", (req, res) => res.send("Ruta clientes medium"));
+app.get("/premium-clients", (req, res) => res.send("Ruta clientes premium"));
+
 app.use("/api/medium-clients", mediumClientsEndpoint);
 app.use("/api/premium-clients", premiumClientsEndpoint);
 
-app.get("/", function (req, res) {
-  res.send("Bienvenido a la api de ADA Cars");
+app.use("/category/:category", (req, res) => {
+  const { category } = req.params;
+  if (["a", "b", "c"].includes(category)) {
+    return res.redirect("/premium-clients");
+  }
+  res.redirect("/medium-clients");
 });
+
+app.get("/", (req, res) => res.send("Bienvenido a la api de ADA Cars"));
 
 module.exports = app;
